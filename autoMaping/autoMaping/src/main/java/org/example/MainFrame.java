7961a1d2f1ef7aace9a99ec8de968bfc15560e29
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-03-08 15:20:36 +0100

    tmp front done - need to connect to db and correct logic

---
+++ autoMaping/autoMaping/src/main/java/org/example/MainFrame.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class MainFrame extends JFrame {
+
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton executeButton;
+    private JButton browseButton1;
+    private JButton browseButton2;
+
+    public MainFrame() {
+        setTitle("Wczytaj plan kont");
+        setSize(450, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        textField1 = new JTextField(10);
+        textField2 = new JTextField(10);
+        textField3 = new JTextField(10);
+        executeButton = new JButton("Wykonaj");
+        browseButton1 = new JButton("Wybierz plik źródłowy");
+        browseButton2 = new JButton("Wybierz plik docelowy");
+
+        add(new JLabel("Plik z kontami"));
+        add(textField1);
+        add(browseButton1);
+        add(new JLabel("Plik ppk"));
+        add(textField2);
+        add(browseButton2);
+        add(new JLabel("Plik ze wzorami kont"));
+        add(textField3);
+        add(executeButton);
+
+        browseButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    textField1.setText(selectedFile.getAbsolutePath());
+                }
+            }
+        });
+
+        browseButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showSaveDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    textField2.setText(selectedFile.getAbsolutePath());
+                }
+            }
+        });
+
+        executeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String konta = textField1.getText();
+                String ppk = textField2.getText();
+                String wzory = textField3.getText();
+                Pojedynczy_rekord main = new Pojedynczy_rekord();
+                try {
+                    main.zaczytaj_wzory(wzory);
+                    main.zaczytaj_dane(konta, ppk);
+                    JOptionPane.showMessageDialog(null, "Operacja zakończona pomyślnie!");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "Wystąpił błąd: " + ex.getMessage());
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+}
--- autoMaping/autoMaping/src/main/java/org/example/Pojedynczy_rekord.java
+++ autoMaping/autoMaping/src/main/java/org/example/Pojedynczy_rekord.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Pojedynczy_rekord {
 
@@ -16,9 +14,11 @@ public class Pojedynczy_rekord {
     private String przemapowany_numer_konta;
     private String konto_syntetyczne;
     public HashMap<String, int[]> wzory;
+    public List<String> pelny_plan_kont;
 
     public Pojedynczy_rekord() {
         wzory = new HashMap<>();
+        pelny_plan_kont = new ArrayList<>();
     }
 
     public void zaczytaj_wzory(String sciezka_do_pliku) throws IOException {
@@ -46,11 +46,12 @@ public class Pojedynczy_rekord {
         plik_wejsciowy_excel.close();
         plik_wejsciowy.close();
     }
-
-    public void zaczytaj_dane(String sciezka_do_pliku) throws IOException {
+    //TODO dsadsadsa
+    public void zaczytaj_dane(String sciezka_do_pliku, String sciezka_do_planu_kont) throws IOException {
         FileInputStream plik_wejsciowy = new FileInputStream((sciezka_do_pliku));
         XSSFWorkbook plik_wejsciowy_excel = new XSSFWorkbook(plik_wejsciowy);
         Sheet pierwszy_arkusz = plik_wejsciowy_excel.getSheetAt(0);
+        zaczytaj_pelny_plan_kont(sciezka_do_planu_kont);
         for (Row row : pierwszy_arkusz) {
             if (row.getRowNum() > 1) {
                 Cell tmp = row.getCell(0);
@@ -63,10 +64,30 @@ public class Pojedynczy_rekord {
                         System.out.println("brak wzoru dla konta:" + konto_syntetyczne + "we wzorach");
                     } else {
                         przemapowany_numer_konta = wypelnij_zerami(obecny_numer_konta, wzory.get(konto_syntetyczne));
-                        System.out.println(przemapowany_numer_konta);
+                        Cell cell = row.createCell(1);
+                        cell.setCellValue(przemapowany_numer_konta);
+                        if (!pelny_plan_kont.contains(przemapowany_numer_konta)) {
+                            CellStyle style = plik_wejsciowy_excel.createCellStyle();
+                            style.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+                            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                            cell.setCellStyle(style);
+                        }
                     }
                 }
             }
+        }
+        plik_wejsciowy_excel.write(new FileOutputStream(new File(sciezka_do_pliku)));
+        plik_wejsciowy_excel.close();
+    }
+
+
+    private void zaczytaj_pelny_plan_kont(String sciezka_do_pliku) throws IOException {
+        FileInputStream plik_wejsciowy = new FileInputStream((sciezka_do_pliku));
+        XSSFWorkbook plik_wejsciowy_excel = new XSSFWorkbook(plik_wejsciowy);
+        Sheet pierwszy_arkusz = plik_wejsciowy_excel.getSheetAt(0);
+        for (Row row : pierwszy_arkusz) {
+            Cell tmp = row.getCell(2);
+            pelny_plan_kont.add(tmp.getStringCellValue());
         }
     }
 
